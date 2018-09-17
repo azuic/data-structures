9d3e23077ba98483fa71cc64928ca63e6af1ab9a
Zui Chen <zuic@umich.edu>
2018-09-17 14:56:09 -0400

    A few more progress on assignment 2

--- week2/parseZone3.js
+++ week2/parseZone3.js
@@ -1,2 +1,10 @@
+// require modules
 var fs = require('fs');
 var cheerio = require('cheerio');
+
+// read zone 3 file
+var content = fs.readFileSync('/Users/zuic/Documents/azuic/data-structures/week1/aa-meeting-list/m03.txt');
+
+// load to an object
+var $ = cheerio.load(content);
+// $ for selecting variables
