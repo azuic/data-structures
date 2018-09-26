f6e7e13b9cb1b26b1de99a14dddee02095551bfd
Zui Chen <zuic@umich.edu>
2018-09-26 16:57:53 -0400

    Finished week 5 assignment.

---
+++ week5/addressExtraction.js
@@ -0,0 +1,34 @@
+// Revision of assignment 2
+
+// require modules
+var fs = require('fs');
+var cheerio = require('cheerio');
+
+// read zone 3 file
+var content = fs.readFileSync('/Users/zuic/Documents/azuic/data-structures/week1_3/aa-meeting-list/m03.txt');
+
+// load to an object
+var $ = cheerio.load(content);
+// $ for selecting variables
+
+// create an array to store all the addresses
+var m03=[];
+
+// [] can take in any attr filter
+// must include everything inside ''
+$('td[style="border-bottom\\:1px solid #e3e3e3; width\\:260px"]').each(
+  function(i,elem){
+    $(this).find('h4,b,br,div,span,img').remove();
+    // remove redundant html elements
+
+    var address = $(this).html().replace(/^\s*$[\n\r]{1,}/gm, '').replace(/\s+/g,' ').trim();
+    // remove empty lines and extra spaces
+
+    m03.push({'address':address});
+    // add each address as a json object to the array
+  }
+);
+
+
+fs.writeFileSync('/Users/zuic/Documents/azuic/data-structures/week5/m03.json', JSON.stringify(m03));
+// save json file
---
+++ week5/geoCoordsExtraction.js
@@ -0,0 +1,56 @@
+//require Modules
+var request = require('request');
+var async = require('async');
+var fs = require('fs');
+
+var apiKey = process.env.TAMU_KEY;
+
+var meetingsData = [];
+var lat
+
+var rawData = fs.readFileSync('/Users/zuic/Documents/azuic/data-structures/week5/m03.json');
+var m03 = JSON.parse(rawData);
+var addresses = m03.map(i => i.address.split(',')[0]);
+//console.log(addresses.length);
+// async.eachSeries(
+//   addresses,
+//
+//   // requesting geo coords from API
+//   function(value, callback) {
+//     var apiRequest = 'https://geoservices.tamu.edu/Services/Geocode/WebService/GeocoderWebServiceHttpNonParsed_V04_01.aspx?';
+//     apiRequest += 'streetAddress=' + value.split(' ').join('%20');
+//     apiRequest += '&city=New%20York&state=NY&apikey=' + apiKey;
+//     apiRequest += '&format=json&version=4.01';
+//     request(
+//       apiRequest,
+//       function(err, resp, body) {
+//         if (err) {
+//           throw err;
+//         } else {
+//             var tamuGeo = JSON.parse(body);
+//             console.log(tamuGeo['FeatureMatchingResultType']);
+//             meetingsData.push(tamuGeo);
+//         }
+//       }
+//     );
+//     setTimeout(callback, 4000); // increase from 2000 to get complete list
+//   },
+//
+//   // write into json file
+//   function() {
+//     fs.writeFileSync('/Users/zuic/Documents/azuic/data-structures/week5/m03Geocodes.json', JSON.stringify(meetingsData));
+//     console.log('*** *** *** *** ***');
+//     console.log('Number of meetings in this zone: ');
+//     console.log(meetingsData.length);
+//   }
+// );
+
+
+var m03Geocodes =   fs.readFileSync('/Users/zuic/Documents/azuic/data-structures/week5/m03Geocodes.json');
+var m03Data = JSON.parse(m03Geocodes);
+
+var m03LatLon = m03Data.map(i => ({'address':i.InputAddress.StreetAddress.slice(0,-13)+", New York, NY", 'latLon':{'Latitude':i.OutputGeocodes[0].OutputGeocode.Latitude,
+          'Longitude':i.OutputGeocodes[0].OutputGeocode.Longitude}}));
+
+
+fs.writeFileSync('/Users/zuic/Documents/azuic/data-structures/week5/m03LatLon.json', JSON.stringify(m03LatLon));
