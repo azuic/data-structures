eec55d2a6def71c2fe79d582ae59ece99b053368
Zui Chen <zuic@umich.edu>
2018-10-04 23:52:51 -0400

    Assignment 4 finished

---
+++ week6/createSqlTable.js
@@ -0,0 +1,29 @@
+// modules
+const { Client } = require('pg');
+var async = require('async');
+var fs = require('fs');
+
+// AWS RDS POSTGRESQL INSTANCE
+var db_credentials = new Object();
+db_credentials.user = 'zuic';
+db_credentials.host = 'zuicdb.ckrsutvzcxxy.us-east-1.rds.amazonaws.com';
+db_credentials.database = 'zuicdb';
+db_credentials.password = process.env.AWSRDS_PW;
+db_credentials.port = 5432;
+
+// Connect to the AWS RDS Postgres database
+const client = new Client(db_credentials);
+client.connect();
+
+// Sample SQL statement to create a table:
+var thisQuery = "CREATE TABLE location_info (address varchar(100), lat double precision, lon double precision);";
+//var thisQuery = "DROP TABLE location_info;";
+// Sample SQL statement to delete a table:
+// var thisQuery = "DROP TABLE aalocations;";
+// Sample SQL statement to query the entire contents of a table:
+// var thisQuery = "SELECT * FROM aalocations;";
+
+client.query(thisQuery, (err, res) => {
+    console.log(err, res);
+    client.end();
+});
---
+++ week6/insertData.js
@@ -0,0 +1,27 @@
+// modules
+const { Client } = require('pg');
+var async = require('async');
+var fs = require('fs');
+
+// AWS RDS POSTGRESQL INSTANCE
+var db_credentials = new Object();
+db_credentials.user = 'zuic';
+db_credentials.host = 'zuicdb.ckrsutvzcxxy.us-east-1.rds.amazonaws.com';
+db_credentials.database = 'zuicdb';
+db_credentials.password = process.env.AWSRDS_PW;
+db_credentials.port = 5432;
+
+var rawData = fs.readFileSync('/Users/zuic/Documents/azuic/data-structures/week5/m03LatLon.json');
+var m03Location = JSON.parse(rawData);
+
+
+async.eachSeries(m03Location, function(value, callback) {
+    const client = new Client(db_credentials);
+    client.connect();
+    var thisQuery = "INSERT INTO location_info VALUES (E'" + value.address + "', " + value.latLon.Latitude + ", " + value.latLon.Longitude + ");";
+    client.query(thisQuery, (err, res) => {
+        console.log(err, res);
+        client.end();
+    });
+    setTimeout(callback, 1000);
+});
