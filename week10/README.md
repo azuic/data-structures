1e459ef8f817e5accb4e1b506ed5b4e8e545ad05
Zui Chen <zuic@umich.edu>
2018-11-01 19:46:27 -0400

    Finished uploading remaining aa meeting data

---
+++ week10/README.md
@@ -0,0 +1,2 @@
+1. Downloaded cleaned data by other team members (1,2,4,6,7,8,10)
+2. Finished uploading all the aa meeting data
---
+++ week10/insertData.js
@@ -0,0 +1,38 @@
+// modules
+const { Client } = require('pg');
+var async = require('async');
+var fs = require('fs');
+
+// AWS RDS POSTGRESQL INSTANCE
+var db_credentials = new Object();
+db_credentials.user = 'zuic';
+db_credentials.host = 'zuicdb.ckrsutvzcxxy.us-east-1.rds.amazonaws.com';
+db_credentials.database = 'aameetings';
+db_credentials.password = process.env.AWSRDS_PW;
+db_credentials.port = 5432;
+
+var rawData = fs.readFileSync('/Users/zuic/Documents/azuic/data-structures/week10/M08CleanData.json');
+var m05 = JSON.parse(rawData);
+
+
+async.eachSeries(m05, function(value, callback) {
+    const client = new Client(db_credentials);
+    client.connect();
+    // var thisQuery = "INSERT INTO aadata VALUES (E"+value.zone+", '"+value.group+"', '"+value.location+"', '"+value.address+"', "+value.latLon.Latitude+", "+value.latLon.Longitude+", '{"+value.day+"}', '{"+value.start+"}', '{"+value.end+"}', '{"+value.type+"}', '{"+value.spinterest+"}', '"+value.wheelchair+"');";
+
+    var thisQuery = "INSERT INTO aainfo ( address,mtgroup,lat,long, mtlocation, wheelchair ,mtdate, mtstart, mtend, mttype,mtspin,mtzone) VALUES (E'"+value.streetAddress+"','"+value.group + "',"+value.latitude+ ","+ value.longitude+ ",'"+ value.mtlocation+"','"+value.wheelchair+ "','{"+ value.day + "}','{" + value.start + "}','{"+ value.end +"}','{"+value.type+"}','{"+ value.spinterest+ "}',"+ value.zone+");";
+    setTimeout(callback, 1000);
+    client.query(thisQuery, (err, res) => {
+        console.log(err, res);
+        client.end();
+    });
+});
+
+// // drop tables
+// const client = new Client(db_credentials);
+// client.connect();
+// var thisQuery = "DROP TABLE aainfo";
+//     client.query(thisQuery, (err, res) => {
+//         console.log(err, res);
+//         client.end();
+//     });
